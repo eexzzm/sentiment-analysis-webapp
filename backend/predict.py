e670ace049431cce91ea2ebffd1e450740b1b69c
eexzzm <reizidankharisma@gmail.com>
2025-06-30 16:12:55 +0700

    add testing with hardcoded text

--- backend/predict.py
+++ backend/predict.py
@@ -1,6 +1,6 @@
 import os
 import joblib
-from .preprocessing import clean_text  # Import the cleaning function
+from preprocessing import clean_text  # Import the cleaning function
 
 MODEL_PATH = os.path.join(os.path.dirname(__file__), '../model/sentiment_pipeline.pkl')
 
@@ -15,4 +15,16 @@ def predict_sentiment(text_list):
     predictions = model.predict(cleaned_texts)
     
     return predictions
-    
+
+
+# for prediction testing with hardcoded text  
+# if __name__ == '__main__':
+#     test_data = [
+#         "Anies sangat bijak menjawab semuanya dengan tenang.",
+#         "Pak Ahok itu kasar banget ngomongnya.",
+#         "Wkwkwk ngaco banget ini debat 😅"
+#     ]
+
+#     preds = predict_sentiment(test_data)
+#     for text, label in zip(test_data, preds):
+#         print(f"{text} → Sentimen: {'Positif' if label == 1 else 'Negatif'}")
